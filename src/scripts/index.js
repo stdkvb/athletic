b6ed30dfcfda62328d5e5614c8302b4ab4f35a82
Bulat Sitdikov <b.sitdikov@wptt.ru>
2023-05-10 16:14:24 +0300

    first screen

---
+++ src/scripts/index.js
@@ -0,0 +1,15 @@
+const burger = document.getElementById('burger')
+const menu = document.getElementById('menu')
+burger.addEventListener('click', () => {
+	menu.classList.toggle('active')
+})
+const close = document.getElementById('close')
+close.addEventListener('click', () => {
+	menu.classList.toggle('active')
+})
+const links = menu.querySelectorAll('a')
+links.forEach((element) => {
+	element.addEventListener('click', () => {
+		menu.classList.toggle('active')
+	})
+});
